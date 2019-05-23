8b034a6190c12cd6d6c881ddbb5ba1312654ff85
codeleeq <princefemibabs@gmail.com>
2019-05-23 15:36:04 +0100

    Added some conditional statement for the 3 buttons

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.musicplayer;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button play,pause,stop;
     MediaPlayer mediaPlayer;
-    int currentPosition;
+    int backButtonCount,currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +29,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
-
+    // this is the app button listener to control play stop and pause.
     @Override
     public void onClick(View view) {
 
         switch(view.getId( )){
 
             case R.id.play_btn:
-                mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.music);
-                mediaPlayer.start();
+                if(mediaPlayer == null) {
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
+                    mediaPlayer.start();
+                }
+                else if (!mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(currentPosition);
+                    mediaPlayer.start();
+
+                }
                 break;
             case R.id.pause_btn:
-                mediaPlayer.pause();
+                if (mediaPlayer != null) {
+                    mediaPlayer.pause();
+                    currentPosition = mediaPlayer.getCurrentPosition();
+                }
                 break;
 
             case R.id.stop_btn:
+                if (mediaPlayer!=null)
                 mediaPlayer.stop();
+                mediaPlayer = null;
                 break;
         }
 
+    }
+    //added backbutton to exit an application
+    @Override
+    public void onBackPressed()
+    {
+        if(backButtonCount >= 1)
+        {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
+            backButtonCount++;
+        }
     }
 }
